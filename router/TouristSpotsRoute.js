72365d25c59120b3187866f8278f3cb84af385de
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-08-05 14:58:53 +0300

    Modification tourist route

--- router/TouristSpotsRoute.js
+++ router/TouristSpotsRoute.js
@@ -1,46 +1,9 @@
 import express from 'express';
-import fs from 'fs';
-import path from 'path';
 import TouristSpots from '../model/TouristSpots.js'; // Make sure to use the correct file path
 
 const router = express.Router();
 
-const videosDirectory = path.join("videos", 'videos');
-const imagesDirectory = path.join("videos", 'images');
 
-// Route to serve individual images
-router.get('/img/:imgname', (req, res) => {
-  const imgName = req.params.imgname;
-  const imgPath = path.join(__dirname, 'images', imgName);
-  const typeMime = imgPath.substring(imgPath.indexOf('.') + 1, imgPath.length);
-
-  fs.access(imgPath, fs.constants.R_OK, (err) => {
-    if (err) {
-      console.error('Error accessing image file:', err);
-      return res.status(404).send('Image not found');
-    }
-
-    res.set('Content-Type', 'image/' + typeMime);
-    res.sendFile(imgPath);
-  });
-});
-
-// Route to serve individual videos
-router.get('/video/:videoName', (req, res) => {
-  const videoName = req.params.videoName;
-  const videoPath = path.join(__dirname, 'videos', videoName);
-  const typeMime = videoPath.substring(videoPath.indexOf('.') + 1, videoPath.length);
-
-  fs.access(videoPath, fs.constants.R_OK, (err) => {
-    if (err) {
-      console.error('Error accessing video file:', err);
-      return res.status(404).send('Video not found');
-    }
-
-    res.set('Content-Type', 'video/' + typeMime);
-    res.sendFile(videoPath);
-  });
-});
 
 // Get the list of tourist spots and details
 router.get('/list', async (req, res) => {
