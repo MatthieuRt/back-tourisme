e764d11c0fa33553973b8cea289ed5a5fb4a9e2b
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-08-05 11:22:57 +0300

    delete

--- model/Categorie.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const CategorieSchema = mongoose.Schema({
-    titre:String,
-    url : String
-}, { collection: 'Categorie' })
-module.exports = mongoose.model('Categorie',CategorieSchema);
--- model/Itineraire.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ItineraireSchema = mongoose.Schema({
-    idUser:String,
-	idTouristSpots :String,
-	idHotel: String,
-}, { collection: 'Itineraire' })
-module.exports = mongoose.model('Itineraire',ItineraireSchema);
