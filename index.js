e40ef827ab39ad02b00cfd2040c13e9fd166c272
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2023-08-05 14:20:57 +0300

    fix- index2

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
 import express from 'express';
-import https from 'https';
-import fs from 'fs';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import './util/DBConnexion.js'; // Make sure to use the correct file path
+import './util/DBConnexion.js';
 
 const app = express();
-const certFilePath = './certificat/certificate.pem';
-const keyFilePath = './certificat/privatekey.pem';
-const passphrase = 'to1rism2ex0mf1nAL';
 
-// Options serveur HTTPS
-const options = {
-    cert: fs.readFileSync(certFilePath),
-    key: fs.readFileSync(keyFilePath),
-    passphrase: passphrase
-};
+app.get('/', (req, res) => {
+    res.send("GG NDAO ATORY SY HOEFUZFNZUDFZUDH");
+});
 
-//const server = https.createServer(options, app);
-
-/*app.use(cors());
-app.use(bodyParser.json());
-
-import TouristSpotRoute from './router/TouristSpotsRoute.js';
-app.use('/touristspots', TouristSpotRoute);
-
-import CategorieRoute from './router/CategorieRoute.js';
-app.use('/categorie', CategorieRoute);
-
-import UserRoute from './router/UtilisateurRoute.js';
-// const InsertionData = require('./util/insertionData');
-// InsertionData();
-app.use('/user', UserRoute);*/
-
-app.listen(5000,()=>{
-    console.log("APP IS RUNNING")
+app.listen(5000, () => {
+    console.log('app running');
 });
